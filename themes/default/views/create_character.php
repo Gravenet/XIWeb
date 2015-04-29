9d5efe45d8bce51a395b891c3096715fe34fd837
ex0r <masteryoer@gmail.com>
2015-04-29 09:12:23 -0400

    UPDATE: Added in the 'Race Faces Reference'

--- themes/default/views/create_character.php
+++ themes/default/views/create_character.php
@@ -71,7 +71,7 @@ $output .= '
             </div>
           </div>
           <div class="uk-form-row">
-            <label class="uk-form-label '.(!empty($_SESSION['validation']['validation']['face']) ? 'uk-text-danger' : '').'"">Face* <a href="#"><i class="uk-icon uk-icon-question-circle"></i></a>:</label>
+            <label class="uk-form-label '.(!empty($_SESSION['validation']['validation']['face']) ? 'uk-text-danger' : '').'"">Face* <a href="#faces" data-uk-modal><i class="uk-icon uk-icon-question-circle"></i></a>:</label>
             <div class="uk-form-controls">
               <div class="uk-grid">
                 <div class="uk-width-1-6">
@@ -184,23 +184,27 @@ $output .= '
         <button class="uk-button uk-button-primary" type="submit">Create Character</button> <button class="uk-button uk-button-danger" type="reset">Clear</button>
       </form>
     </div>
-    
-    <!-- This is the off-canvas sidebar -->
-    <div id="my-id" class="uk-offcanvas">
-      <div class="uk-offcanvas-bar uk-offcanvas-bar-flip">
-      <div class="uk-panel">
-        <h3 class="uk-panel-title">Online Friends (1)</h3>
-        <span><a href="#" class="uk-text-primary"><i class="uk-icon uk-icon-user-plus"></i> Add friend</a></span>
-        <hr class="uk-panel-divider" />
-        <i class="uk-icon uk-icon-toggle-on uk-text-success"></i> <a href="#my-modal"  data-uk-modal>Friend 1</a> <i class="uk-icon uk-icon-times uk-text-danger"></i>
-        <hr class="uk-panel-divider" />
-        <h3 class="uk-panel-title">Offline Friends (4)</h3>
-        <hr class="uk-panel-divider" />
-        <em class="uk-text-muted"><i class="uk-icon uk-icon-toggle-off"></i> Friend 2 <i class="uk-icon uk-icon-times uk-text-danger"></i></em><br />
-        <em class="uk-text-muted"><i class="uk-icon uk-icon-toggle-off"></i> Friend 3 <i class="uk-icon uk-icon-times uk-text-danger"></i></em><br />
-        <em class="uk-text-muted"><i class="uk-icon uk-icon-toggle-off"></i> Friend 4 <i class="uk-icon uk-icon-times uk-text-danger"></i></em><br />
-        <em class="uk-text-muted"><i class="uk-icon uk-icon-toggle-off"></i> Friend 5 <i class="uk-icon uk-icon-times uk-text-danger"></i></em><br />
-        <hr class="uk-panel-divider" />
+    <div id="faces" class="uk-modal">
+      <div class="uk-modal-dialog">
+          <a class="uk-modal-close uk-close"></a>
+          <div class="uk-panel-title"><i class="uk-icon uk-icon-eye"></i> Character Faces</div>
+          <hr class="uk-panel-divider" />
+          <table style="width: 100%">
+          <tr>
+            <td><div class="style1"><strong><span>- Elvaan -<br>
+              <img alt="Elvaan" src="themes/default/images/Elvaan%20Faces.jpg" width="957" height="415"></span></strong></div>
+            <div><hr><br><strong><span>- Hume -<br>
+              <img alt="Hume" src="themes/default/images/Hume%20Faces.jpg" width="960" height="415"></span></strong><br></div>
+            <div><hr><br><strong><span>- Tarutaru -<br>
+              <img alt="Tarutaru" src="themes/default/images/Tarutaru%20Faces.jpg" width="960" height="416"></span></strong><br></div>
+            <div><hr><br><strong><span>- Mithra -<br>
+              <img alt="Mithra" src="themes/default/images/Mithra%20Faces.jpg" width="981" height="211"></span></strong><br></div>
+            <div><hr><br><strong><span>- Galka -<br>
+              <img alt="Galka" src="themes/default/images/Galka%20Faces.jpg" width="984" height="212"></span></strong><br></div>
+            <div><hr></div>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
 ';
